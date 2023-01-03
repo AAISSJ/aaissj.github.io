fb1aa94fd2ef7d097bd2ec812162ade8d693bc1e
Sejung Son <76966915+AAISSJ@users.noreply.github.com>
2023-01-03 22:48:16 +0900

    Update utils.jsx

--- src/helpers/utils.jsx
+++ src/helpers/utils.jsx
@@ -162,7 +162,7 @@ export const sanitizeConfig = (config) => {
       fileUrl: config?.resume?.fileUrl || '',
     },
     skills: config?.skills || [],
-
+    interests: config?.interests || [],
     externalProjects: config?.externalProjects || [],
     publication: config?.publication || [],
     experiences: config?.experiences || [],
