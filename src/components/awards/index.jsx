6bce0142de0096453183c5ea883482eef9c382af
Sejung Son <76966915+AAISSJ@users.noreply.github.com>
2023-01-04 00:13:38 +0900

    Update index.jsx

--- src/components/awards/index.jsx
+++ src/components/awards/index.jsx
@@ -142,7 +142,7 @@ const Award = ({ awards, loading, googleAnalytics }) => {
                         skeleton({ width: 'w-40', height: 'h-8' })
                       ) : (
                         <span className="text-base-content opacity-70">
-                          Awards
+                          Honors & Awards
                         </span>
                       )}
                     </h5>
