d8831e8b089e0322a42fe0d3205bffdb088352d1
Sejung Son <76966915+AAISSJ@users.noreply.github.com>
2023-01-03 22:28:06 +0900

    Create index.jsx

---
+++ src/components/interest/index.jsx
@@ -0,0 +1,60 @@
+import { skeleton } from '../../helpers/utils';
+import PropTypes from 'prop-types';
+
+const Interest = ({ loading, interests }) => {
+  const renderSkeleton = () => {
+    let array = [];
+    for (let index = 0; index < 12; index++) {
+      array.push(
+        <div key={index}>
+          {skeleton({ width: 'w-16', height: 'h-4', className: 'm-1' })}
+        </div>
+      );
+    }
+
+    return array;
+  };
+
+  return (
+    <>
+      {interests?.length !== 0 && (
+        <div className="card shadow-lg compact bg-base-100">
+          <div className="card-body">
+            <div className="mx-3">
+              <h5 className="card-title">
+                {loading ? (
+                  skeleton({ width: 'w-32', height: 'h-8' })
+                ) : (
+                  <span className="text-base-content opacity-70">
+                    Tech Stack
+                  </span>
+                )}
+              </h5>
+            </div>
+            <div className="p-3 flow-root">
+              <div className="-m-1 flex flex-wrap justify-center">
+                {loading
+                  ? renderSkeleton()
+                  : interests.map((interest, index) => (
+                      <div
+                        key={index}
+                        className="m-1 text-xs inline-flex items-center font-bold leading-sm px-3 py-1 badge-primary bg-opacity-90 rounded-full"
+                      >
+                        {interest}
+                      </div>
+                    ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+Interest.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  interests: PropTypes.array.isRequired,
+};
+
+export default Interest;
