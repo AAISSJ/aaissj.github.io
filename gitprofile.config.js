d880bc11d50dfdf0c698a27249cc9881f3212b6a
Sejung Son <76966915+AAISSJ@users.noreply.github.com>
2023-01-13 13:06:16 +0900

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -94,6 +94,14 @@ const config = {
     },
   ],
    awards: [
+     {
+      title: 'Full Tuition Scholarship (granted for full semesters)',
+      description:
+        '2022.08 ~ 2024.02 , Sungkyunkwan University',
+      imageUrl: 'https://images.unsplash.com/photo-1579389083078-4e7018379f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+      link: 'https://example.com',
+      
+    },
     {
       title: 'Summa Cum Laude, Honors BA in AAI',
       description:
