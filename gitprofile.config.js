fd07d18b5a2bc4df73e40a6ad4198ce47798d383
Sejung Son <aaissj17@gmail.com>
2023-05-17 13:02:27 +0900

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -100,7 +100,7 @@ const config = {
   ],
   publication: [
     {
-      title: 'Towards Suicide Prevention from Bipolar Disorder with Temporal Symptom-Aware Multitask Learning',
+      title: 'Towards Suicide Prevention from Bipolar Disorder with Temporal Symptom-Aware Multitask Learning (KDD, 2023)',
       description:
         'Daeun Lee, Sejung Son, Hyolim Jeon, Chaewon Park, Jihyun An, Seungbae Kim, and Jinyoung Han*, In ACM SIGKDD, Aug. 2023.',
       imageUrl: 'https://images.unsplash.com/photo-1620585837584-5e50abfc24c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
