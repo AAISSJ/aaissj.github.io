b1c811279d4b80d6990dcfa3f540ed540c710e35
Sejung Son <aaissj17@gmail.com>
2023-05-20 21:10:21 +0900

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -115,7 +115,7 @@ const config = {
       link: 'https://kdd.org/kdd2023/',
     },
     {
-      title: 'A Multilingual Multimodal Model for Diagnosing Dementia /<br>//<br>/',
+      title: 'A Multilingual Multimodal Model for Diagnosing Dementia &nbsp  &nbsp',
       description:
         'Sejung Son, Daeun Lee, and Jinyoung Han*, In Korea Computer Congress, Jun. 2023.',
       imageUrl: 'https://images.unsplash.com/photo-1559757175-5700dde675bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80',
