26aa014de996e9d83b794a5d5b2a7a4774d8d77f
Sejung Son <aaissj17@gmail.com>
2024-01-16 13:11:29 +0900

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -152,7 +152,7 @@ const config = {
   ],
    awards: [
     {
-      title: 'Best Researcher Awards',
+      title: '🏆 Best Researcher Awards',
       description:
         'January 2024, Department of Applied Artificial Intelligence, Sungkyunkwan University(SKKU)',
       imageUrl: 'https://images.unsplash.com/photo-1578269174936-2709b6aeb913?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
