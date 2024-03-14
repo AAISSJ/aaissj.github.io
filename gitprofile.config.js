8871b135b5254709e86c2d387668e6bac2ad2560
Sejung Son <aaissj17@gmail.com>
2024-03-14 18:39:23 +0900

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -290,7 +290,7 @@ const config = {
     // Available themes. To remove any theme, exclude from here.
     themes: [
       'light',
-      'dark',
+      // 'dark',
       'cupcake',
       'bumblebee',
       'emerald',
